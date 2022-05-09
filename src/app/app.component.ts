b76adfa39d6fc9367700f6c2bb4bfc1970f2afd5
velrino <denismagalhaesvelrino@gmail.com>
2022-05-09 14:03:37 -0300

    Implemented socket io

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,39 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { WebSocketService } from './services/websoket.service';
+import { HttpClient } from '@angular/common/http';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'websocket-client-side';
+  public users: number = 0;
+  public message: string = '';
+  public messages: string[] = [];
+
+  constructor(
+    private http: HttpClient,
+    private webSocketService: WebSocketService
+  ) {
+  }
+
+  ngOnInit() {
+    this.http.get<any>('http://localhost:3000/api/currency').subscribe(data => {
+      console.log(data)
+    })
+    this.webSocketService.receiveChat().subscribe((message: any) => {
+      this.messages.push(message);
+    });
+    this.webSocketService.getUsers().subscribe((users: any) => {
+      this.users = users;
+    });
+  }
+
+  addChat() {
+    this.messages.push(this.message);
+    this.webSocketService.sendChat(this.message);
+    this.message = '';
+  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,8 +1,13 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { FormsModule } from '@angular/forms';
+import { HttpClientModule } from '@angular/common/http';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { SocketIoModule, SocketIoConfig } from 'ngx-socket-io';
+
+const config: SocketIoConfig = { url: 'http://localhost:3000', options: {} };
 
 @NgModule({
   declarations: [
@@ -10,7 +15,10 @@ import { AppComponent } from './app.component';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    HttpClientModule,
+    AppRoutingModule,
+    FormsModule,
+    SocketIoModule.forRoot(config),
   ],
   providers: [],
   bootstrap: [AppComponent]
